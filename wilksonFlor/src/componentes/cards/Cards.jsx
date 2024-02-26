2c9b43528f819a0d9304fe71a701858fd5f15392
Wilksonflor <wilksonflor12@gmail.com>
2024-02-26 20:46:43 -0300

    Projeto finalizado

--- wilksonFlor/src/componentes/cards/Cards.jsx
+++ wilksonFlor/src/componentes/cards/Cards.jsx
@@ -4,6 +4,8 @@ import img3 from "../../assets/produtos/air-jordan-1-retro-high-court-purple-w-1
 import img4 from "../../assets/produtos/air-jordan-1-mid-grey-camo-1-1000 1.png";
 import img5 from "../../assets/produtos/air-jordan-1-mid-light-smoke-grey-gs-1-1000.png";
 import img6 from "../../assets/produtos/air-jordan-1-mid-bright-citrus-1-1000 1.png";
+import img7 from "../../assets/produtos/JordanBlack.png";
+import img8 from "../../assets/produtos/Jordan-Shoes-PNG-Images.png";
 
 const produtosList = [
   {
@@ -48,34 +50,60 @@ const produtosList = [
     modelo: "Tênis Air Jordan",
     preco: "999,99",
   },
+  {
+    id: 7,
+    img: img7,
+    nome: "Air Jordan Black",
+    modelo: "Tênis Air Jordan",
+    preco: "1399,90",
+  },
+  {
+    id: 8,
+    img: img8,
+    nome: "Air Jordan Shoes",
+    modelo: "Tênis Air Jordan",
+    preco: "1249,90",
+  },
+  // {
+  //   id: 8,
+  //   img: img8,
+  //   nome: "Air Jordan IV",
+  //   modelo: "Tênis Air Jordan",
+  //   preco: "1249,90",
+  // },
 ];
 
 export const Cards = () => {
   return (
-    <div className="containerCards">
-      <div className="textTCards">
-        <h2>Os melhores em só lugar</h2>
-        <p>
-          A marca Jordan na JordanShoes é a escolha certa para os amantes de
-          sneakers que buscam estilo e conforto.
-        </p>
+    <>
+      <div className="containerCards">
+        <div className="textTCards">
+          <h2>Os melhores em só lugar</h2>
+          <p>
+            A marca Jordan na JordanShoes é a escolha certa para os amantes de
+            sneakers que buscam estilo e conforto.
+          </p>
+        </div>
+
+        <div className="cardsContainer">
+          {produtosList.map((data, index) => (
+            <div key={index} className="cards">
+              <div className="bgImg">
+                <img src={data.img} alt={data.nome} />
+              </div>
+
+              <div className="text">
+                <p className="nome">{data.nome}</p>
+                <p className="modelo">{data.modelo}</p>
+                <p className="preco">R$ {data.preco}</p>
+                <button>Selecionar</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
 
-      <div className="cardsContainer">
-        {produtosList.map((data, index) => (
-          <div key={index} className="cards">
-            <div className="bgImg">
-              <img src={data.img} alt={data.nome} />
-            </div>
-
-            <div className="text">
-              <p className="nome">{data.nome}</p>
-              <p className="modelo">{data.modelo}</p>
-              <p className="preco">R$ {data.preco}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+      <footer>Todos os direitos reservados a Wilkson Flor &copy;</footer>
+    </>
   );
 };
